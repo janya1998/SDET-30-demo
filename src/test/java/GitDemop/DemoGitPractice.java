1132c59ff7f095f734ffaf33be49d1b9119b7232
soujanya <ssoujanya998@gmail.com>
2022-03-10 16:08:13 +0530

    user 1: first commit

---
+++ src/test/java/GitDemop/DemoGitPractice.java
@@ -0,0 +1,12 @@
+package GitDemop;
+
+import org.testng.annotations.Test;
+
+public class DemoGitPractice {
+
+	@Test
+	public void demoGitPractice()
+	{
+	System.out.println(" User1 : fist comit");
+}
+}
